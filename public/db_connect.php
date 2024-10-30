97650eb0afda9dbb0c7ce76277b2cd0643bc1068
Dareencmu <snsd9@live.com>
2024-10-30 14:51:32 +0700

    Your commit message

--- public/db_connect.php
+++ public/db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "hotel_booking";
+$servername = "srv1649.hstgr.io";  // Update this to match the host in your JavaScript
+$username = "u786554873_lantaaj";  // Update this to match the username in your JavaScript
+$password = "Chiangmai1929!";  // Update this to match the password in your JavaScript
+$dbname = "u786554873_hotel_booking";  // Update this to match the database name in your JavaScript
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
